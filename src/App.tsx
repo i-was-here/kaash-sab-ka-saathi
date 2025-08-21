b3356ad180f3fda5fa9559a6bf351af8ab20542a
i-was-here <nirmitarora26@gmail.com>
2025-08-21 19:06:27 +0000

    fix the 404 error and remove dist folder

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ const App = () => (
       <Sonner />
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Index />} />
+          <Route path="/kaash-sab-ka-saathi/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? "/kaash-sab-ka-saathi/" : "/",
+  base: "/kaash-sab-ka-saathi/",
   server: {
     host: "::",
     port: 8080,
