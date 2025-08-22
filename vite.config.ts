86374090337a5f2ec8a7c92ae864d33ce7fa2161
tpavankalyan <tankalapavankalyan@gmail.com>
2025-08-22 19:26:37 +0530

    site works

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/kaash-sab-ka-saathi/",
+  base: "/",
   server: {
     host: "::",
     port: 8080,
